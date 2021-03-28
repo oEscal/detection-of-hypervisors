446cfe90f88baa4e2dd1c585fb8726f943f03aa3
oEscal <pedroescaleira@hotmail.com>
2021-03-28 23:00:08 +0100

    makefile and it is working and some documentation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # detection-of-hypervisors
+
+## Initial work
+
+ - First, we just run a simple test for `cpuinfo` and mesure the time between the beginning and the end of the instruction. We also, for accuracy reasons, run our simple program on the same thread of the same CPU core (in this case, the one with the index 0), using the Linux command `taskset`:
+    
+    ```bash
+    $ taskset -c 0 ./test
+    ```
--- code/test.c
+++ code/test.c
@@ -2,40 +2,37 @@
 
 
 unsigned long first_one_bit(unsigned long word) {
-    register unsigned long result;
-    asm volatile("bsfq %[data],%[result]": [result] "=r"(result): [data]   "r"(word));
-    return result;
+	register unsigned long result;
+	asm volatile("bsfq %[data],%[result]": [result] "=r"(result): [data]   "r"(word));
+	return result;
 }
 
 unsigned long cpu() {
-    register unsigned long result;
+	register unsigned long result;
 
-    asm volatile("cpuid"
-    :
-    :
-    );
-    return result;
+	asm volatile("cpuid"
+	:
+	:
+	);
+	return result;
 }
 
-unsigned long rdtsc() {
-    unsigned long rax, rcx, rdx;
+unsigned long rdtsc(void) {
+	unsigned long rax, rcx, rdx;
 
-    asm volatile("rdtscp"
-    : "=a" (rax), "=c" (rcx), "=d" (rdx)
-    );
-    printf("%lu\n", rcx);
-    printf("%lu\n", rdx);
-    printf("%lu\n", rax);
-    return (rdx << 32) + rax;
+	asm volatile("rdtscp"
+	: "=a" (rax), "=c" (rcx), "=d" (rdx)
+	);
+	return (rdx << 32) + rax;
 }
 
 
 int main() {
-    unsigned long t0, t1;
+	unsigned long t0, t1;
 
-    t0 = rdtsc();
-    first_one_bit(1);
-    t1 = rdtsc();
+	t0 = rdtsc();
+	cpu();
+	t1 = rdtsc();
 
-    printf("%lu %lu %lu\n", t0, t1, t1 - t0);
+	printf("%lu %lu %lu\n", t0, t1, t1 - t0);
 }
