1d2cb219501caad6d9d50f64e4f8766b8968178e
oEscal <escaleira@manjaro.lan>
2021-04-27 18:55:03 +0100

    host intel kernel model results and small fix

--- code/kernel/run_several_times.sh
+++ code/kernel/run_several_times.sh
@@ -13,9 +13,10 @@ sudo insmod cicles_counter.ko
 echo "echo $1 > /sys/module/cicles_counter/parameters/number_runs" | sudo bash
 
 for i in $(seq 0 $((${#INSTRUCTIONS[@]} - 1))); do
+   echo "************************************************************************"
    echo "Running for the instruction <${INSTRUCTIONS[i]}> (id=$i)"
    echo "echo $i > /sys/module/cicles_counter/parameters/instruction" | sudo bash
    taskset -c 0 cat /proc/$MODULE_NAME > results/$2_${INSTRUCTIONS[i]}
 
-   python3 interpret_results.py $2_${INSTRUCTIONS[i]}
+   python3 interpret_results.py --file $2_${INSTRUCTIONS[i]}
 done
