8b82106d133b23a9d35a9822c72a7130a3962390
oEscal <escaleira@manjaro.lan>
2021-05-02 13:24:43 +0100

    virtualbox intel kernel mode results

--- code/user/interpret_results.py
+++ code/user/interpret_results.py
@@ -1,14 +1,27 @@
-instruction_data = []
-xor_data = []
+import argparse
 
-with open("results/host_amd.txt", "r") as file:
-   for l in file.readlines():
-      current = l.split(" ")
-      xor_data.append(int(current[0]))
-      instruction_data.append(int(current[1]))
 
-print(min(xor_data))
-print(min(instruction_data))
+def main(args):
+   instruction_data = []
+   xor_data = []
 
-print(f"Minimum of clock cycles: {min(instruction_data)/min(xor_data)}")
-print(f"Maximum of clock cycles: {max(instruction_data)/min(xor_data)}")
+   with open(f"results/{args.file}", "r") as file:
+      for l in file.readlines():
+         current = l.split(" ")
+         xor_data.append(int(current[0]))
+         instruction_data.append(int(current[1]))
+
+   print(min(xor_data))
+   print(min(instruction_data))
+
+   print(f"Minimum of clock cycles: {min(instruction_data)/min(xor_data)}")
+   print(f"Maximum of clock cycles: {max(instruction_data)/min(xor_data)}")
+
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--file', default="host_amd.txt", type=str, help="Result's file were to read from")
+   args = parser.parse_args()
+
+   main(args)
+
--- code/user/run_several_times.sh
+++ code/user/run_several_times.sh
@@ -3,3 +3,4 @@
 BINARY_NAME=main
 
 taskset -c 0 ./$BINARY_NAME $1 > results/$2
+python3 interpret_results.py --file $2
